8fc8f9ee8fc615ed11404e55cecaead131988460
Siddharth Bhagwat <sidvicious@Siddharths-MacBook-Pro.local>
2023-07-24 23:06:00 -0700

    first commit

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+
+import state from '../store';
+
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  // Set anisotropy
+  if (logoTexture) logoTexture.anisotropy = 16;
+  if (fullTexture) fullTexture.anisotropy = 16;
+
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+
+  const stateString = JSON.stringify(snap);
+
+  return nodes && materials ? (
+    <group key={stateString}>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+        {snap.isFullTexture && (
+          <Decal 
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={[1, 1, 1]}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal 
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={[0.15, 0.15, 0.15]}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
+    </group>
+  ) : null;  // Or replace `null` with a loading spinner or similar
+}
+
+export default Shirt;
